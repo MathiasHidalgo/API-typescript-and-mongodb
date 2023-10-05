489de804a0bdc67cbe519c2a8b809809ac92089a
MathiasHidalgo <mejhc2307lbc@gmail.com>
2023-10-05 13:04:12 -0400

    server init, start with mongo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
+// Importing every module we'll use in the app
+
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import dotenv from 'dotenv'; dotenv.config();
+
+//init the application
+
+const app = express();
+const port = process.env.PORT || 8080;
+//Configuring all the middlewares
+
+
+app.use(cors({
+	credentials: true,
+}))
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(port, () =>{
+	console.log(`Server Running on port http://localhost:${port}/`);
+})
+
